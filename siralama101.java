eb43e59fe01022516ea2cd566b731f308915b795
Coskann <alicoskandemirbolat@gmail.com>
2021-06-16 02:07:25 +0300

    Homework

--- siralama101.java
+++ siralama101.java
@@ -15,29 +15,29 @@ public class siralama101 {
 
         c = scan.nextInt();
 
-        if((a>b)&&(a>c)){
-            if(b>c){
+        if ((a > b) && (a > c)) {
+            if (b > c) {
                 System.out.print("a>b>c");
 
-                }else{
+            } else {
                 System.out.println("a>c>b");
             }
 
-            }else if((b>a)&&(b>c)){
-            if(a>c){
+        } else if ((b > a) && (b > c)) {
+            if (a > c) {
                 System.out.println("b>a>c");
 
 
-            }else{
+            } else {
                 System.out.println("b>c>a");
             }
-        }else if ((c>a)&&(c>b)){
-            if(a>b){
+        } else if ((c > a) && (c > b)) {
+            if (a > b) {
                 System.out.println("c>a>b");
-            }else{
+            } else {
                 System.out.println("c>b>a");
             }
         }
-        }
-
     }
+
+}
