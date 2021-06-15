123def280c854cb66eb01e019ee0e46f48e0c636
Coskann <alicoskandemirbolat@gmail.com>
2021-06-15 23:37:10 +0300

    etkinlik101

---
+++ etkinlik101/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double heat;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hava sıcaklığını giriniz :");
+        heat = input.nextDouble();
+        if (heat > 25) {
+            System.out.println("Yüzme etkinliklerini öner");
+        } else if (heat > 5 && heat < 15) {
+            System.out.println("Sinema önerilir");
+        } else if (heat < 5) {
+            System.out.println("Kayma etkinlikleri önerilir.");
+        } else if (10<heat&&heat<25) {
+            System.out.println("Piknik Önerilir.");
+        }
+    }
+}
