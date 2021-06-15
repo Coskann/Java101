112e78e102682ba8ce2303990f14c3a06b396489
Coskann <alicoskandemirbolat@gmail.com>
2021-06-16 02:06:17 +0300

    siralama101

---
+++ siralama101.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class siralama101 {
+
+    public static void main(String[] args) {
+        int a, b, c;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Sayıları Giriniz.");
+
+        a = scan.nextInt();
+
+        b = scan.nextInt();
+
+        c = scan.nextInt();
+
+        if((a>b)&&(a>c)){
+            if(b>c){
+                System.out.print("a>b>c");
+
+                }else{
+                System.out.println("a>c>b");
+            }
+
+            }else if((b>a)&&(b>c)){
+            if(a>c){
+                System.out.println("b>a>c");
+
+
+            }else{
+                System.out.println("b>c>a");
+            }
+        }else if ((c>a)&&(c>b)){
+            if(a>b){
+                System.out.println("c>a>b");
+            }else{
+                System.out.println("c>b>a");
+            }
+        }
+        }
+
+    }
